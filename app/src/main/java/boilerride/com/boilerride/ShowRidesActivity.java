f3b33c9d016d3667314325d3f473388008e7e735
Nadeem Mahmood <mahmoodn@purdue.edu>
2016-03-04 12:48:21 -0500

    retrieving rides from db

--- app/src/main/java/boilerride/com/boilerride/ShowRidesActivity.java
+++ app/src/main/java/boilerride/com/boilerride/ShowRidesActivity.java
@@ -80,10 +80,10 @@ public class ShowRidesActivity extends AppCompatActivity{
         // Set up the login form.
 
         list = (ListView) findViewById(R.id.showrides_listView);
-        ArrayList arrayList = new ArrayList<Ride>();
         // Adapter: You need three parameters 'the context, id of the layout (it will be where the data is shown),
         // and the array that contains the data
-        ArrayAdapter adapter = new ArrayAdapter<Ride>(getApplicationContext(), android.R.layout.simple_spinner_item, arrayList){
+        RideList rl = new RideList();
+        ArrayAdapter adapter = new ArrayAdapter<Ride>(getApplicationContext(), android.R.layout.simple_spinner_item, rl.getRides()){
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
                 View view = super.getView(position, convertView, parent);
@@ -96,21 +96,6 @@ public class ShowRidesActivity extends AppCompatActivity{
         // Here, you set the data in your ListView
         list.setAdapter(adapter);
 
-
-
-        Ride ride1 = new Ride(1, 2, 200, "Purdue Union", "Chicago", 5, "12 am", "2 pm", "10 pm",
-                "Ride to Chicago airport", false);
-        Ride ride2  = new Ride(1, 3, 100, "Purdue Airport", "Chicago", 5, "12 am", "2 pm", "10 pm",
-                "Ride to Chicago downtown", false);
-        Ride ride3  = new Ride(1, 2, 200, "Purdue Union", "Indy", 5, "12 am", "2 pm", "10 pm",
-                "Ride to Indy", false);
-        Ride ride4  = new Ride(1, 2, 200, "Purdue Union", "FortWayne", 5, "12 am", "2 pm", "10 pm",
-                "Ride to FortWayne", false);
-
-        arrayList.add(ride1);
-        arrayList.add(ride2);
-        arrayList.add(ride3);
-        arrayList.add(ride4);
         // next thing you have to do is check if your adapter has changed
         adapter.notifyDataSetChanged();
 
