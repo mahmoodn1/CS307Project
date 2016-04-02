e3aea1ff37e690090c6b91ed4bb7d4eb3254ae74
Nadeem Mahmood <mahmoodn@purdue.edu>
2016-04-01 22:15:53 -0400

    notification and payment updates

--- app/src/main/java/boilerride/com/boilerride/ShowRidesActivity.java
+++ app/src/main/java/boilerride/com/boilerride/ShowRidesActivity.java
@@ -13,6 +13,8 @@ import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -20,35 +22,51 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.view.ViewGroup;
+import android.widget.AdapterView.OnItemClickListener;
+import android.view.*;
+import android.widget.TimePicker;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.firebase.client.Query;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
-/**
- * A login screen that offers login via email/password.
- */
 public class ShowRidesActivity extends AppCompatActivity{
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
+
+    public Firebase myFirebase;
+    private ArrayList<Ride> listofRides = new ArrayList <Ride>();
+    public int counter = 0;
 
     /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
+     * Keep track of the login task to ensure we can cancel it if requested.
      */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
+    private GetRideTask mAuthTask = null;
+    ArrayAdapter adapter;
 
     // UI references.
+    private AutoCompleteTextView mDestView;
+    private EditText mTitleView;
+
     private ListView list;
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -79,11 +97,16 @@ public class ShowRidesActivity extends AppCompatActivity{
         setContentView(R.layout.activity_showrides);
         // Set up the login form.
 
+        counter = 1;
+        Firebase.setAndroidContext(this);
+        myFirebase  = new Firebase("https://luminous-torch-1510.firebaseio.com/rides");
+        attemptPull();
+
         list = (ListView) findViewById(R.id.showrides_listView);
+
         // Adapter: You need three parameters 'the context, id of the layout (it will be where the data is shown),
         // and the array that contains the data
-        RideList rl = new RideList();
-        ArrayAdapter adapter = new ArrayAdapter<Ride>(getApplicationContext(), android.R.layout.simple_spinner_item, rl.getRides()){
+        adapter = new ArrayAdapter<Ride>(getApplicationContext(), android.R.layout.simple_spinner_item, listofRides) {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
                 View view = super.getView(position, convertView, parent);
@@ -96,8 +119,20 @@ public class ShowRidesActivity extends AppCompatActivity{
         // Here, you set the data in your ListView
         list.setAdapter(adapter);
 
+        /*Ride ride1 = new Ride(1, 2, 200, "Purdue Union", "Chicago", 5, "12 am", "2 pm", "10 pm",
+                "Ride to Chicago airport", true);
+
+        rides.add(ride1);*/
         // next thing you have to do is check if your adapter has changed
-        adapter.notifyDataSetChanged();
+
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
+                Intent intent = new Intent(getApplicationContext(), RideActivity.class);
+                startActivity(intent);
+            }
+        });
 
         FloatingActionButton myFab = (FloatingActionButton)  findViewById(R.id.showrides_fab);
         myFab.setOnClickListener(new View.OnClickListener() {
@@ -105,10 +140,39 @@ public class ShowRidesActivity extends AppCompatActivity{
                 startCreateRideActivity();
             }
         });
-
-
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    /**
+     * Attempts to post ride specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual post attempt is made.
+     */
+    private void attemptPull() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            mAuthTask = new GetRideTask();
+            mAuthTask.execute((Void) null);
+        }
+    }
 
     private void startCreateRideActivity()
     {
@@ -153,5 +217,142 @@ public class ShowRidesActivity extends AppCompatActivity{
         startActivity(intent);
 
     }
-}
 
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+/*            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });*/
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            // mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            // mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class GetRideTask extends AsyncTask<Void, Void, Boolean> {
+        private boolean type; // 0 = offer 1 = request
+        private double numOfPassengers;
+        private double fare;
+        private double distance;
+        private String origin;
+        private String destination;
+        private double maxPassengers;
+        private String departTime;
+        private String arrivalTime;
+        private String timePosted;
+        private String title;
+
+        public GetRideTask() {
+        }
+
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                //Query queryRef = myFirebase.orderByChild("timePosted");
+                // Attach an listener to read the data at our rides reference
+                myFirebase.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        System.out.println("There are " + snapshot.getChildrenCount() + " rides");
+                        for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                            listofRides.clear();
+                            //Ride ride = postSnapshot.getValue(Ride.class);
+                            String rideString = postSnapshot.getValue().toString();
+                            String[] rideA = rideString.split(",");
+                            String value;
+                            for (int i = 0; i < rideA.length - 1; i++) {
+                                rideA[i] = rideA[i].substring(rideA[i].indexOf("=") + 1);
+                            }
+                            rideA[rideA.length - 1] = rideA[rideA.length - 1].substring(rideA[rideA.length - 1].indexOf("=") + 1);
+                            rideA[rideA.length - 1] = rideA[rideA.length - 1].substring(0, rideA[rideA.length - 1].indexOf("}"));
+
+
+                            double numOfPassengers = Double.valueOf(rideA[8]);
+                            double fare = Double.valueOf(rideA[10]);
+                            double distance = Double.valueOf(rideA[7]);
+                            String origin = rideA[6];
+                            String destination = rideA[2];
+                            double maxPassengers = Double.valueOf(rideA[5]);
+                            String departTime = rideA[3];
+                            String arrivalTime = rideA[0];
+                            String timePosted = rideA[1];
+                            String title = rideA[4];
+                            String type1 = rideA[9];
+                            boolean type;
+                            if (type1.equals("offer"))
+                                type = false;
+                            else
+                                type = true;
+
+
+                            Ride ride = new Ride(numOfPassengers, fare, distance, origin, destination, maxPassengers, departTime, arrivalTime,
+                                    timePosted, title, type);
+                            listofRides.add(ride);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(FirebaseError firebaseError) {
+                        System.out.println("The read failed: " + firebaseError.getMessage());
+                    }
+                });
+                // Simulate network access.
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            // TODO: register the new account here.
+            return true;
+        }
+
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            showProgress(false);
+
+        }
+
+        protected void onCancelled() {
+            mAuthTask = null;
+            showProgress(false);
+        }
+    }
+
+    private void startShowRidesActivity()
+    {
+        Intent intent = new Intent(this, ShowRidesActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
