06be2bf79fa670f83609606223c7bd795867ed38
k0n1star <konstantinpandl@yahoo.de>
2016-04-29 15:29:02 -0400

    Fixing logout, prepare maintain session

--- app/src/main/java/boilerride/com/boilerride/LoginActivity.java
+++ app/src/main/java/boilerride/com/boilerride/LoginActivity.java
@@ -7,12 +7,14 @@ import android.app.LoaderManager.LoaderCallbacks;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -101,6 +103,17 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         });
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+
+        SharedPreferences prefs = getSharedPreferences("text", 0);
+        String email = prefs.getString("email", null);
+        String password = prefs.getString("pw", null);
+        if (email!=null && password!=null) {
+            // etc
+            mEmailView.setText(email);
+            mPasswordView.setText(password);
+        }
+
     }
 
     public void registerAccount(View v) {
@@ -198,6 +211,16 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
+
+            SharedPreferences prefs = getSharedPreferences("text", 0);
+            email = mEmailView.getText().toString();
+            password = mPasswordView.getText().toString();
+
+            SharedPreferences.Editor edit = prefs.edit();
+            edit.putString("email", email);
+            edit.putString("pw", password);
+            edit.commit();
+
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
